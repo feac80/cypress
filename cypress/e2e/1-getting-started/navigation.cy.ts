a068dd075a36e4870e7939e54f175022ab330d33
Frank Araque Cadenas <frank.araquecadenas@infor.com>
2022-11-19 11:53:39 +0100

    increase scroll bar duration to 3000

--- cypress/e2e/1-getting-started/navigation.cy.ts
+++ cypress/e2e/1-getting-started/navigation.cy.ts
@@ -24,7 +24,7 @@ describe('example navigate to portfolio page', () => {
     // We use the `cy.get()` command to get all elements that match the selector.
     // Then, we use `should` to assert that there are two matched items,
     // which are the two default items.
-   cy.scrollTo('bottom', { duration: 5000 })
+   cy.scrollTo('bottom', { duration: 3000 })
 
 
   })
